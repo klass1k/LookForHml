03bf9af2a25e2917bbde1d02c4a708fd9cfb64b5
klass1k <klas1k003@gmail.com>
2017-12-06 18:47:18 +0300

    Add files via upload

--- LookForHml/LookFor.cs
+++ LookForHml/LookFor.cs
@@ -9,26 +9,29 @@ namespace LookForHml
 {
     public class LookFor
     { 
-        public IEnumerable<string> GetHtml(string directoryPath)
+        public IEnumerable<FileInfo> GetHtml(string directoryPath)
         {
-            if(directoryPath == null)
-            {
-                yield break;
-            }
-            else
-            {
-                yield return directoryPath;
+            //if(directoryPath == null)
+            //{
+            //    yield break;
+            //}
+            //else
+            //{
+                //yield return directoryPath;
                 DirectoryInfo dir = new DirectoryInfo(directoryPath);
-                foreach (var f in dir.EnumerateFiles("*.html"))
-                {
-                    yield return f.Name;
-                }
-                
-                foreach (var get in GetHtml(Path.GetDirectoryName(directoryPath)))// переход на уровень ниже
-                {
-                    yield return get;
-                }
-            }                           
+                return dir.EnumerateFiles("*.html", SearchOption.AllDirectories);
+                    
+                //foreach (var f in dir.EnumerateFiles("*.html"))
+                //{
+                //    yield return f.Name;
+                //}
+
+                //foreach (var get in GetHtml(Path.GetDirectoryName(directoryPath)))
+                //{
+                //    yield return get;
+                //}
+            //}
+
         }       
     }
 }
