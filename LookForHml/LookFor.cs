8ecbea02681784c6020a3e61a9ac76346820a5a0
klass1k <klas1k003@gmail.com>
2017-12-06 15:05:09 +0300

    Initial commit

---
+++ LookForHml/LookFor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookForHml
+{
+    public class LookFor
+    {
+        private List<string> allhtml = new List<string>();
+        
+        private string directoryName;
+        public IEnumerable<string> GetHtml(string path)
+        {
+            DirSearch(path);
+            for (int i = 0; i < allhtml.Count; i++)
+            {
+                yield return allhtml[i];
+            }
+        }
+        private void DirSearch(string filePath)
+        {
+            directoryName = Path.GetDirectoryName(filePath);
+            if (filePath != null)
+            {
+                DirectoryInfo dir = new DirectoryInfo(filePath);
+                FileInfo[] files = dir.GetFiles("*.html");
+                foreach (var f in files)
+                {
+                    allhtml.Add(f.FullName);
+                }
+            }
+            allhtml.Add(filePath);
+            filePath = directoryName;            
+           
+            if (filePath != null)
+            {
+                this.DirSearch(filePath);
+            }
+        }
+    }
+}
+        
---
+++ LookForHml/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookForHml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LookFor lf = new LookFor();
+            //DirSearch(@"D:\English\pre\New English File Class CD Pre-Intermediate (mp3 split up version)(2005)");
+            string filePath = @"D:\English\pre";           
+            foreach (var item in lf.GetHtml(filePath))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
+
