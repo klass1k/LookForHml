e157a9d8161d68c06dd2710d497f0299a9a41391
klass1k <klas1k003@gmail.com>
2017-12-06 19:27:52 +0300

    test2

--- LookForHml/LookFor.cs
+++ LookForHml/LookFor.cs
@@ -8,31 +8,68 @@ using System.Threading.Tasks;
 namespace LookForHml
 {
     public class LookFor
-    { 
-        public IEnumerable<FileInfo> GetHtml(string directoryPath)
+    {
+        private int t = 0;
+        public List<string> FullnameSucssefull = new List<string>();
+        public List<string> FullnameFail = new List<string>();
+        public IEnumerable<string> GetHtml(string directoryPath)
         {
-            //if(directoryPath == null)
-            //{
-            //    yield break;
-            //}
-            //else
-            //{
-                //yield return directoryPath;
+            if (directoryPath == null)
+            {
+                yield break;
+            }
+            else
+            {
+                yield return directoryPath + $"\t{t}level";
+                t++;
                 DirectoryInfo dir = new DirectoryInfo(directoryPath);
-                return dir.EnumerateFiles("*.html", SearchOption.AllDirectories);
-                    
-                //foreach (var f in dir.EnumerateFiles("*.html"))
-                //{
-                //    yield return f.Name;
-                //}
+                foreach (var f in dir.EnumerateFiles("*.html"))
+                {
+                    yield return f.Name;
+                }
 
-                //foreach (var get in GetHtml(Path.GetDirectoryName(directoryPath)))
-                //{
-                //    yield return get;
-                //}
-            //}
+                foreach (var get in GetHtml(Path.GetDirectoryName(directoryPath)))
+                {
+                    yield return get;
+                }
+            }
+        }
+        private void FindInDir(DirectoryInfo dir, string pattern, bool recursive)
+        {
+            try
+            {
+                foreach (FileInfo file in dir.GetFiles(pattern))
+                {
+                    FullnameSucssefull.Add(file.FullName);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
 
-        }       
+            if (recursive)
+            {
+                DirectoryInfo[] subdir = dir.GetDirectories();
+                int i;
+                int l = subdir.Length;
+                for (i = 1; i < l; i++)
+                {
+                    try
+                    {
+                        this.FindInDir(subdir[i], pattern, recursive);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        FullnameFail.Add(subdir[i].Name);
+                    }
+                }
+            }
+        }
+
+        public void FindFiles(string dir, string pattern)
+        {            
+            this.FindInDir(new DirectoryInfo(dir), pattern, true);
+        }
     }
 }
         
