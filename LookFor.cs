daa1b5661bb81b378e80534ea64dcbf343ca6926
klass1k <klas1k003@gmail.com>
2017-12-06 16:57:38 +0300

    optimal

---
+++ LookFor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookForHml
+{
+    public class LookFor
+    { 
+        public IEnumerable<string> GetHtml(string filePath)
+        {
+            if(filePath==null)
+            {
+                yield break;
+            }
+            else
+            {
+                yield return filePath;
+                DirectoryInfo dir = new DirectoryInfo(filePath);
+                FileInfo[] files = dir.GetFiles("*.html");
+                foreach (var f in files)
+                {
+                    yield return f.Name;
+                }
+                
+                foreach (var get in GetHtml(Path.GetDirectoryName(filePath)))
+                {
+                    yield return get;
+                }
+            }               
+        }       
+    }
+}
+        
